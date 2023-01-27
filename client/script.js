53abe68cb69d31dcb9be08fdf616cda3472e363b
darkghost33 <deveshmukherjee76@gmail.com>
2023-01-28 04:44:33 +0530

    server deployed

--- client/script.js
+++ client/script.js
@@ -77,7 +77,7 @@ const handleSubmit = async(e) => {
   loader(messageDiv);
 
   //fetch data from server =====
-  const response = await fetch('http://localhost:5000',{
+  const response = await fetch('https://talkbot-the-ai-chatbot.onrender.com',{
     method: 'POST',
     headers: {
       'Content-Type' : 'application/json'
